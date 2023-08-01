8ada72e42c7978f67377f52d0a958b198d228c0f
ScopeH-github <hongsch07@gmail.com>
2023-08-01 23:29:06 +0900

    Initial Commit

---
+++ bullsAndCows_SwiftUI/bullsAndCows_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  bullsAndCows_SwiftUIApp.swift
+//  bullsAndCows_SwiftUI
+//
+//  Created by Scope.H (Work) on 2023/08/01.
+//
+
+import SwiftUI
+
+@main
+struct bullsAndCows_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
