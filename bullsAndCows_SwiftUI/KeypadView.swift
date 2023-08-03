8f0d7256e8837f8c1b9cae700fb3a9d716d60cea
ScopeH-github <hongsch07@gmail.com>
2023-08-03 21:34:01 +0900

    make keypad ButtonExample and modified View Modifiers

--- bullsAndCows_SwiftUI/KeypadView.swift
+++ bullsAndCows_SwiftUI/KeypadView.swift
@@ -10,15 +10,42 @@ import SwiftUI
 struct KeyPadTextViewModifier: ViewModifier {
     func body(content: Content) -> some View {
         content
+            .monospaced(true)
             .monospacedDigit()
-            .frame(minWidth: 0, maxWidth: 70, minHeight: 0, maxHeight: 65)
+            .font(.title2)
+            .frame(minWidth: 0, maxWidth: 65, minHeight: 0, maxHeight: 70)
     }
 }
 
-struct KeyPadButtonVireModifier: ViewModifier {
+struct KeyPadButtonViewModifier: ViewModifier {
     func body(content: Content) -> some View {
         content
             .buttonBorderShape(.capsule)
             .buttonStyle(.bordered)
+    }
+}
+
+extension View {
+    func keypadButtonViewStyle() -> some View {
+        modifier(KeyPadButtonViewModifier())
+    }
+    func keypadLabelViewStyle() -> some View {
+        modifier(KeyPadTextViewModifier())
+    }
+}
+
+struct KeyPadView: View {
+    var body: some View {
+        Button (action: {}) {
+            Image(systemName: "clear.fill")
+                .keypadLabelViewStyle()
+        }
+        .keypadButtonViewStyle()
+    }
+}
+
+struct KeyPadView_Previews: PreviewProvider {
+    static var previews: some View {
+        KeyPadView()
     }
 }
