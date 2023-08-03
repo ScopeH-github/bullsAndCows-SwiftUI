4b3c9dc07ac3b7232f2e814e1acda2b3bdeeaaad
ScopeH-github <hongsch07@gmail.com>
2023-08-03 23:53:25 +0900

    parameter fix

--- bullsAndCows_SwiftUI/bullsAndCows_SwiftUIApp.swift
+++ bullsAndCows_SwiftUI/bullsAndCows_SwiftUIApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct bullsAndCows_SwiftUIApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView(password: "123456")
         }
     }
 }
