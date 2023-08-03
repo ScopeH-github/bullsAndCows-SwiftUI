684d3336722b4488ea97e329867a75841711c51f
ScopeH-github <hongsch07@gmail.com>
2023-08-03 21:13:08 +0900

    Button View Modifier

--- bullsAndCows_SwiftUI/KeypadView.swift
+++ bullsAndCows_SwiftUI/KeypadView.swift
@@ -5,4 +5,20 @@
 //  Created by Scope.H (Work) on 2023/08/03.
 //
 
-import Foundation
+import SwiftUI
+
+struct KeyPadTextViewModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .monospacedDigit()
+            .frame(minWidth: 0, maxWidth: 70, minHeight: 0, maxHeight: 65)
+    }
+}
+
+struct KeyPadButtonVireModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .buttonBorderShape(.capsule)
+            .buttonStyle(.bordered)
+    }
+}
